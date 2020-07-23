a18a259045d916c6250f0020737ef27a6fb26072
hzy <1193457593@qq.com>
2020-07-23 17:47:09 +0800

    提交

--- src/navigator/HomeStackNavigator.js
+++ src/navigator/HomeStackNavigator.js
@@ -1,62 +1,64 @@
-import { createStackNavigator, TransitionPresets } from '@react-navigation/stack';
+import {createStackNavigator, TransitionPresets} from '@react-navigation/stack';
 import Icon from 'react-native-vector-icons/Ionicons';
-import { HeaderBackButton } from '@react-navigation/stack';
-import { Animated } from 'react-native';
+import AntDesignIcon from 'react-native-vector-icons/AntDesign';
+import {HeaderBackButton} from '@react-navigation/stack';
+import {Animated} from 'react-native';
 import * as React from 'react';
+import LinearGradient from 'react-native-linear-gradient';
 
 import HomeScreen from '../pages/home/home';
 import DetailScreen from '../pages/home/detail';
 
 const Stack = createStackNavigator();
 
-const forFade = ({ current, next }) => {
-    const opacity = Animated.add(
-        current.progress,
-        next ? next.progress : 0,
-    ).interpolate({
-        inputRange: [0, 1, 2],
-        outputRange: [0, 1, 0],
-    });
-
-    return {
-        leftButtonStyle: { opacity },
-        rightButtonStyle: { opacity },
-        titleStyle: { opacity },
-        backgroundStyle: { opacity },
-    };
-};
-
 export default class HomeStackScreen extends React.Component {
-    render() {
-        return (
-            <Stack.Navigator initialRouteName="Home" mode="modal">
-                <Stack.Screen
-                    name="Home"
-                    component={HomeScreen}
-                    options={{
-                        headerTintColor: '#000',
-                        headerTitleAlign: 'center',
-                        headerStyle: {
-                            backgroundColor: 'tomato',
-                        },
-                    }}
+  render() {
+    return (
+      <Stack.Navigator initialRouteName="Home" mode="modal">
+        <Stack.Screen
+          name="玉汇缘"
+          component={HomeScreen}
+          options={{
+            headerTintColor: '#fff',
+            headerTitleAlign: 'left',
+            headerBackground: () => {
+              return (
+                <LinearGradient
+                  start={{x: 0, y: 0}}
+                  end={{x: 0, y: 1}}
+                  style={{flex: 1}}
+                  colors={['#9BD6D2', '#4CDBC5']}
                 />
-                <Stack.Screen
-                    name="Detail"
-                    options={{
-                        headerTintColor: '#000',
-                        headerTitleAlign: 'center',
-                        headerStyle: {
-                            backgroundColor: 'tomato',
-                        },
-                        headerShown: true,
-                        headerBackImage: () => {
-                            return <Icon name="add-circle" size={20} color="red" />
-                        },
-                    }}
-                    component={DetailScreen}
+              );
+            },
+            headerRight: () => {
+              return (
+                <AntDesignIcon
+                  name="message1"
+                  size={20}
+                  color="white"
+                  style={{marginRight: 10}}
                 />
-            </Stack.Navigator>
-        );
-    }
+              );
+            },
+          }}
+        />
+        <Stack.Screen
+          name="Detail"
+          options={{
+            headerTintColor: '#000',
+            headerTitleAlign: 'center',
+            headerStyle: {
+              backgroundColor: 'tomato',
+            },
+            headerShown: true,
+            headerBackImage: () => (
+              <Icon name="chevron-back" size={40} color="white" />
+            ),
+          }}
+          component={DetailScreen}
+        />
+      </Stack.Navigator>
+    );
+  }
 }
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,24 +1,140 @@
-import { Text, View, Button } from 'react-native';
+import {Text, View, StyleSheet, ImageBackground, Image} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import React from 'react';
-export default class Home extends React.Component {
-    render() {
-        return (
-            <>
-                <View>
-                    <Text>222222222222</Text>
-                    <Text><Icon name="add-circle" size={20} color="red" /></Text>
+import {Carousel} from '@ant-design/react-native';
+import Swiper from 'react-native-swiper';
+import AntDesignIcon from 'react-native-vector-icons/AntDesign';
 
-                    <Button
-                        title="gotodetail"
-                        onPress={() => this.props.navigation.navigate('Detail')}
-                    />
-                    <Button
-                        title="gotologin"
-                        onPress={() => this.props.navigation.navigate('Login')}
-                    />
-                </View>
-            </>
-        );
-    }
+export default class Home extends React.Component {
+  render() {
+    return (
+      <View style={styles.box}>
+        <ImageBackground
+          source={{uri: 'https://zh-hans.reactjs.org/logo-og.png'}}
+          style={[styles.image, {position: 'absolute'}]}
+        />
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+            height: 140,
+          }}>
+          <View style={{width: 345, height: 140}}>
+            <Swiper
+              height={200} // 指定高度
+              loop={true} // 是否开启循环
+              showsButtons={false} // 设置控制按钮(左右两侧的箭头)是否可见
+              index={0}
+              autoplay={true} // 是否自动跳转
+              horizontal={true} // 是否水平滚动
+            >
+              <View
+                style={[styles.containerHorizontal, {backgroundColor: 'blue'}]}>
+                <Text>Carousel 1</Text>
+              </View>
+              <View
+                style={[styles.containerHorizontal, {backgroundColor: 'blue'}]}>
+                <Text>Carousel 2</Text>
+              </View>
+              <View
+                style={[
+                  styles.containerHorizontal,
+                  {backgroundColor: 'yellow'},
+                ]}>
+                <Text>Carousel 3</Text>
+              </View>
+              <View
+                style={[styles.containerHorizontal, {backgroundColor: 'aqua'}]}>
+                <Text>Carousel 4</Text>
+              </View>
+            </Swiper>
+          </View>
+        </View>
+        <View style={styles.nav}>
+          <View style={styles.navList}>
+            <Image
+              style={styles.tinyLogo}
+              source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}}
+            />
+            <Text>社区联盟</Text>
+          </View>
+          <View style={styles.navList}>
+            <Image
+              style={styles.tinyLogo}
+              source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}}
+            />
+            <Text>新人手册</Text>
+          </View>
+          <View style={styles.navList}>
+            <Image
+              style={styles.tinyLogo}
+              source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}}
+            />
+            <Text>分享邀请</Text>
+          </View>
+          <View style={styles.navList}>
+            <Image
+              style={styles.tinyLogo}
+              source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}}
+            />
+            <Text>玉石链圈</Text>
+          </View>
+        </View>
+        <View style={styles.announce}>
+          <View
+            style={{
+              width: 345,
+              height: 28,
+              backgroundColor: '#fff',
+              flexDirection: 'row',
+              alignItems: 'center',
+            }}>
+            <AntDesignIcon
+              name="sound"
+              size={17}
+              color="#F8908A"
+              style={{marginRight: 10}}
+            />
+            <Text>55555</Text>
+          </View>
+        </View>
+      </View>
+    );
+  }
 }
+const styles = StyleSheet.create({
+  box: {
+    backgroundColor: '#f5f5f5',
+  },
+  wrapper: {
+    backgroundColor: '#000',
+  },
+  containerHorizontal: {
+    height: 200,
+    width: 350,
+  },
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+    width: 500,
+    height: 100,
+  },
+  nav: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 20,
+    height: 65,
+  },
+  navList: {
+    width: 60,
+    height: 65,
+    fontSize: 12,
+    backgroundColor: 'red',
+  },
+  announce: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginTop: 20,
+  },
+});
